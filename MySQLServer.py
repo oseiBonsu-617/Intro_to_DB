36fc4532afa3d04731142a38c5d8430d99b0eca5
nobbonsu <nobbonsu440@gmail.com>
2025-10-21 11:09:44 +0000

    python script on databases

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Nobbonsu617$",
+    database = "alx_book_store"
+)
+
+cursor = db.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+print("Database 'alx_book_store' created successfully!")
+
+# Close connection
+cursor.close()
+db.close()
